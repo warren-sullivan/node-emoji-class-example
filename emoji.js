be5e6c5b6d32cadd7acbdc7b6b23ddab897baab1
Warren Sullivan <warren@sullivantek.com>
2017-08-14 20:29:39 -0500

    remote emoji repo

--- emoji.js
+++ emoji.js
@@ -1,17 +1,46 @@
-let emoji = require('node-emoji');
+const emoji = require('node-emoji');
+const https = require('https');
+const http = require('http');
 
-let array = [];
-array.push(emoji.get('coffee'));
-array.push(emoji.get('pizza'));
-array.push(emoji.get('fist'));
-array.push(emoji.get('sun_small_cloud'));
-array.push(emoji.get('closed_lock_with_key'));
-array.push(emoji.get('headphones'));
-array.push(emoji.get('mouse2'));
-array.push(emoji.get('recycle'));
-array.push(emoji.get('maple_leaf'));
-array.push(emoji.get('steam_locomotive'));
-
-for(let i=0; i<array.length; i++) {
-	console.log(array[i]);
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
+
+let data;
+let array = [];
+
+const server = http.createServer((req, res) => {
+	res.write(array[getRandomInt(0, array.length)]);
+	res.end();
+});
+
+const options = {
+  host: 'raw.githubusercontent.com',
+  port: 443,
+  path: '/omnidan/node-emoji/master/lib/emoji.json',
+  method: 'GET'
+};
+
+let req = https.request(options, (res) => {
+	res.setEncoding('utf8');
+	res.on('data', (chunk) => {
+		if(!data) {data = chunk}
+		else {data += chunk}
+	});
+	res.on('end', () => {
+		data = JSON.parse(data);
+		for(i in data) {
+			array.push(data[i]);
+		}
+
+		// for(i in array) {
+		// 	console.log(array[i]);
+		// }
+
+		server.listen(8001);
+	});
+});
+
+req.end();
